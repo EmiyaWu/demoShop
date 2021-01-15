e8bfd6cfd24730543457a93ac01365de800e73e4
EmiyaWu <fate36541@gmail.com>
2021-01-15 17:47:30 +0800

    新增bootstrap網格系統以及簡易HEAD

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from "react";
+import { Container, Row, Col } from 'styled-bootstrap-grid';
 import styled from "styled-components";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <HeadText>123</HeadText>
+        <Container>
+          <HeadContainer>
+            <a>Home</a>
+            <div>ShopCard</div>
+          </HeadContainer>
+
+          <Row row>
+            <TestCol col>1-1</TestCol>
+          </Row>
+        </Container>
       </div>
+      
     )
   }
 }
 
 export default App;
 
-const HeadText = styled.span`
-    color: #000;
-`;
+
+const TestCol = styled(Col)`
+  border: solid 1px black;
+`
+
+const HeadContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 15px;
+`
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {GridThemeProvider} from 'styled-bootstrap-grid';
+import bootstrapGridTheme from './gridTheme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GridThemeProvider
+      gridTheme={bootstrapGridTheme}
+    >
+      <App />
+    </GridThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

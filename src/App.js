bddf75f36514e69fbe5f89946c0dba3a3ccff299
RoyWu <roy_wu@myplay20.com>
2021-01-19 10:54:33 +0800

    新增商品結構且將cache加入除外版控

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React from "react";
 import { Container, Row, Col } from 'styled-bootstrap-grid';
 import styled from "styled-components";
 
+const commodityData = [
+  {id:1, commodityName: "原味飯糰", price: 30, commodityimg:""},
+  {id:2, commodityName: "火腿飯糰", price: 35, commodityimg:""},
+  {id:3, commodityName: "起司飯糰", price: 40, commodityimg:""},
+  {id:4, commodityName: "肉球飯糰", price: 55, commodityimg:""},
+  {id:5, commodityName: "總和飯糰", price: 60, commodityimg:""}
+]
+
+
 class App extends React.Component {
   render() {
     return (
@@ -9,14 +18,28 @@ class App extends React.Component {
         <Container>
           <HeadContainer>
             <a>Home</a>
-            <div>ShopCard</div>
+            <div>ShopCard[0]</div>
           </HeadContainer>
 
-          <Row row>
-            <TestCol col>
-              <div>1-1</div>
-              <div>1-2</div>
-            </TestCol>
+          <CustomizeRow>
+            <IntroductionCol col>
+              <IntroductionContainer>
+                <IntroductionSubject>Hi, Welcome 我的商城</IntroductionSubject>
+                <IntroductionSubtitle>目前商城正在特價中，所有商品一律八折!</IntroductionSubtitle>
+              </IntroductionContainer>
+            </IntroductionCol>
+          </CustomizeRow>
+
+          <Row>
+            {commodityData.map((data) =>  
+              <Col col={3} key={data.id}>
+                <Test>
+                  <div>{data.commodityName}</div>
+                  <div>$ {data.price}</div>
+                  <button>加入購物車!</button>
+                </Test>
+            </Col>
+            )}
           </Row>
         </Container>
       </div>
@@ -27,10 +50,35 @@ class App extends React.Component {
 
 export default App;
 
-
-const TestCol = styled(Col)`
+const Test = styled.div`
   border: solid 1px black;
-`
+  border-radius: 5px;
+  margin-bottom: 10px;
+  padding: 5px;
+`;
+
+const IntroductionSubtitle = styled.div`
+  margin: 5px 0 0 0;
+  font-size: 12px;
+`;
+
+const IntroductionSubject = styled.div`
+  margin: 0 0 5px 0;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const IntroductionContainer = styled.div`
+  border: solid 1px gray;
+  border-radius: 10px;
+  padding: 10px 5px;
+`;
+
+const IntroductionCol = styled(Col)``;
+
+const CustomizeRow = styled(Row)`
+  margin-bottom: 15px;
+`;
 
 const HeadContainer = styled.div`
   display: flex;

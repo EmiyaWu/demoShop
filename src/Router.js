8c7e4b014e06414ead4988815908bea1bf1f960d
EmiyaWu <fate36541@gmail.com>
2021-05-04 12:13:30 +0800

    新增路由設定

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 import Home from './Home/Home';
 import ShopCar from './shopCar/ShopCar';
 
 class Router extends Component {
   render() {
     return (
-      <div>
+      <BrowserRouter basename={ process.env.PUBLIC_URL }>
         <Route path="/" exact component={Home} />
         <Route path="/ShopCar" component={ShopCar} />
-      </div>
+      </BrowserRouter>
     );
   }
 }
